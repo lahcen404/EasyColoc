913c3161d6ba082ae81b2342ba92f813ec53ff0b
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-24 11:10:47 +0000

    create Category Model

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function expenses(){
+        return $this->hasMany(Expense::class);
+    }
+    
 }
