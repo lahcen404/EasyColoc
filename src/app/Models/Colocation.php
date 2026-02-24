aedf113fed7baa4aaf6bc4b36b3c642b99d9a687
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-24 10:21:23 +0000

    Create model Colocation

--- src/app/Models/Colocation.php
+++ src/app/Models/Colocation.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Enums\ColocationStatus;
 use Illuminate\Database\Eloquent\Model;
 
 class Colocation extends Model
 {
-    protected $fillable = [];
+    protected $fillable = ['name','status'];
+
+    protected $casts = ['status' => ColocationStatus::class,];
+
+    // relationss
+    public function memberships(){
+        return $this->hasMany(Membership::class);
+    }
+
+    public function expenses(){
+        return $this->hasMany(Expense::class);
+    }
 }
