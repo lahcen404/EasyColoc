03479f0adedddd3c117835037c81f8c0c62c8e5e
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-24 11:07:50 +0000

    create Membership & Expense  Model with relations

--- src/app/Models/Expense.php
+++ src/app/Models/Expense.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = ['title','amount','date','payer_id',
+                            'colocation_id','category_id'];
+
+    protected $casts = [];
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- src/app/Models/Membership.php
+++ src/app/Models/Membership.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Membership extends Model
 {
-    //
+    protected $fillable = [ 'user_id','colocation_id',
+                             'reputation_score','is_owner',
+                            'joined_at','left_at'];
+
+
+    protected $casts = ['is_owner' => 'boolean',
+                        'joined_at' => 'datetime',
+                        'left_at' => 'datetime',];
+
+    // relations
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation(){
+        return $this->belongsTo(Colocation::class);
+    }
+
 }
