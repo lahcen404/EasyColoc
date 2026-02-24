2e1151b0139ba1eed0570ae381696f2ecd3ad521
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-24 09:42:14 +0000

    create users migration

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\UserRole;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,9 +16,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('role')->default(UserRole::USER->value);
+            $table->boolean('is_banned')->default(false);
             $table->timestamps();
         });
 
