f3f8d6568a6125dd99f5eb0e4bc585570bc2a3a4
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-25 22:09:33 +0000

    Expense

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\Member\MemberDashboardController;
+use App\Http\Controllers\Member\ExpenseController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,30 +12,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [MemberDashboardController::class, 'index'])->name('dashboard');
 
-    // Admin
-    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-
-    // User
-    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
-    Route::post('/admin/users/{user}/toggle-ban', [UserController::class, 'toggleBan'])->name('admin.users.toggle-ban');
-
-    // Colocation
+    // colocation routes
     Route::get('/colocation/create', [ColocationController::class, 'create'])->name('colocations.create');
     Route::post('/colocation/create', [ColocationController::class, 'store'])->name('colocations.store');
 
+    // expeense routes
+    Route::get('/expenses/create', [ExpenseController::class, 'create'])->name('expenses.create');
+    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
 
-    Route::get('/dashboard', [MemberDashboardController::class, 'index'])->name('dashboard');
-
-
+    // profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    // admin routes
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::post('/users/{user}/toggle-ban', [UserController::class, 'toggleBan'])->name('users.toggle-ban');
+    });
 });
 
 require __DIR__.'/auth.php';
